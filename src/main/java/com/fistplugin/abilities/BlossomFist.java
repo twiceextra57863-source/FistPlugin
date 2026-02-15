b203fd09496f55e89b00704ac2d49e690ad0f150
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-15 08:15:10 +0530

    Update BlossomFist.java

--- src/main/java/com/fistplugin/abilities/BlossomFist.java
+++ src/main/java/com/fistplugin/abilities/BlossomFist.java
@@ -1,15 +1,15 @@
 package com.fistplugin.abilities;
 
 import com.fistplugin.FistPlugin;
-import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
-import org.bukkit.entity.*;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
 
 public class BlossomFist extends BaseAbility {
     
@@ -19,11 +19,9 @@ public class BlossomFist extends BaseAbility {
     
     @Override
     public boolean onRightClick(Player player) {
-        // Launch snowball projectile
         Snowball snowball = player.launchProjectile(Snowball.class);
         snowball.setVelocity(player.getLocation().getDirection().multiply(2.5));
         
-        // Track projectile
         new BukkitRunnable() {
             @Override
             public void run() {
@@ -31,25 +29,18 @@ public class BlossomFist extends BaseAbility {
                     cancel();
                     return;
                 }
-                
-                // Flower particles
-                Location loc = snowball.getLocation();
-                loc.getWorld().spawnParticle(Particle.HAPPY_VILLAGER, loc, 2, 0.1, 0.1, 0.1, 0);
-                loc.getWorld().spawnParticle(Particle.END_ROD, loc, 1, 0.1, 0.1, 0.1, 0);
+                snowball.getWorld().spawnParticle(Particle.HAPPY_VILLAGER, snowball.getLocation(), 2, 0.1, 0.1, 0.1, 0);
             }
         }.runTaskTimer(plugin, 0L, 1L);
         
-        // Add hit listener
         plugin.getServer().getPluginManager().registerEvents(new org.bukkit.event.Listener() {
             @org.bukkit.event.EventHandler
             public void onProjectileHit(org.bukkit.event.entity.ProjectileHitEvent event) {
                 if (event.getEntity().equals(snowball) && event.getHitEntity() instanceof LivingEntity) {
                     LivingEntity target = (LivingEntity) event.getHitEntity();
                     
-                    // Freeze effect
-                    target.setFreezeTicks(80); // 4 seconds
+                    target.setFreezeTicks(80);
                     
-                    // Visual effect
                     new BukkitRunnable() {
                         int ticks = 0;
                         
@@ -59,15 +50,11 @@ public class BlossomFist extends BaseAbility {
                                 cancel();
                                 return;
                             }
-                            
                             target.getWorld().spawnParticle(Particle.ITEM_SNOWBALL, 
                                 target.getLocation().add(0, 1, 0), 3, 0.3, 0.3, 0.3, 0);
                             ticks += 5;
                         }
                     }.runTaskTimer(plugin, 0L, 5L);
-                    
-                    // Sound
-                    target.getWorld().playSound(target.getLocation(), Sound.BLOCK_POWDER_SNOW_STEP, 1.0f, 0.5f);
                 }
             }
         }, plugin);
@@ -87,14 +74,11 @@ public class BlossomFist extends BaseAbility {
         Location start = player.getLocation().add(0, 1, 0);
         Location end = target.getLocation().add(0, 1, 0);
         
-        // Chain effect
         spawnLineParticles(start, end, Particle.END_ROD, 0.3);
         
-        // Freeze and poison
-        target.setFreezeTicks(200); // 10 seconds
+        target.setFreezeTicks(200);
         target.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 200, 1));
         
-        // Damage over time
         new BukkitRunnable() {
             int damageTicks = 0;
             
@@ -109,21 +93,20 @@ public class BlossomFist extends BaseAbility {
                 target.getWorld().spawnParticle(Particle.DAMAGE_INDICATOR, 
                     target.getLocation().add(0, 1, 0), 5, 0.3, 0.3, 0.3, 0);
                 
-                // Spiral particles
                 for (int i = 0; i < 360; i += 45) {
                     double rad = Math.toRadians(i + damageTicks * 20);
                     double x = Math.sin(rad) * 1.5;
                     double z = Math.cos(rad) * 1.5;
                     
+                    // FIXED: SPELL_MOB -> ENTITY_EFFECT
                     Location spiral = target.getLocation().clone().add(x, 1 + Math.sin(rad) * 0.5, z);
-                    target.getWorld().spawnParticle(Particle.SPELL_MOB, spiral, 1, 0, 0, 0, 0);
+                    target.getWorld().spawnParticle(Particle.ENTITY_EFFECT, spiral, 1, 0, 0, 0, 0);
                 }
                 
                 damageTicks++;
             }
-        }.runTaskTimer(plugin, 20L, 20L); // Every second for 10 seconds
+        }.runTaskTimer(plugin, 20L, 20L);
         
-        // Sound
         player.getWorld().playSound(player.getLocation(), Sound.ENTITY_EVOKER_CAST_SPELL, 1.0f, 1.0f);
         
         plugin.getFistManager().getPlayerData(player).addAbilityUsed();
