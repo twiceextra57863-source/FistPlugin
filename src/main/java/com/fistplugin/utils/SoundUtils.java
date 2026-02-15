454eb9954a37b14ba40705850cd9a7ea5d5a9b3c
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-15 08:18:33 +0530

    Update SoundUtils.java

--- src/main/java/com/fistplugin/utils/SoundUtils.java
+++ src/main/java/com/fistplugin/utils/SoundUtils.java
@@ -1,230 +1,69 @@
 package com.fistplugin.utils;
 
 import com.fistplugin.FistPlugin;
-import com.fistplugin.data.FistType;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class SoundUtils {
     
-    private static FistPlugin plugin;
+    private final FistPlugin plugin;
     
-    public static void init(FistPlugin instance) {
-        plugin = instance;
+    public SoundUtils(FistPlugin plugin) {
+        this.plugin = plugin;
     }
     
-    /**
-     * Play ability sound based on fist type
-     */
-    public static void playAbilitySound(Location loc, FistType fist, boolean isCrouch) {
-        switch(fist) {
-            case ORB:
-                if (isCrouch) {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_ENDER_DRAGON_GROWL, 1.0f, 0.8f);
-                } else {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_FIREWORK_ROCKET_BLAST, 1.0f, 1.0f);
-                }
-                break;
-                
-            case BLOSSOM:
-                if (isCrouch) {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_EVOKER_CAST_SPELL, 1.0f, 1.2f);
-                } else {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_SNOWBALL_THROW, 1.0f, 1.5f);
-                }
-                break;
-                
-            case BEAST:
-                if (isCrouch) {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_ENDERMAN_TELEPORT, 1.0f, 0.5f);
-                } else {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_RAVAGER_ROAR, 1.0f, 1.0f);
-                }
-                break;
-                
-            case WATER:
-                if (isCrouch) {
-                    loc.getWorld().playSound(loc, Sound.ITEM_BUCKET_EMPTY, 2.0f, 0.5f);
-                } else {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_PLAYER_SPLASH, 1.0f, 1.2f);
-                }
-                break;
-                
-            case REALITY:
-                if (isCrouch) {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_WITHER_SHOOT, 1.0f, 0.5f);
-                } else {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_GENERIC_EXPLODE, 1.0f, 0.5f);
-                }
-                break;
-                
-            case COSMIC:
-                if (isCrouch) {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_FISHING_BOBBER_THROW, 1.0f, 0.5f);
-                } else {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_ENDER_DRAGON_FLAP, 1.0f, 1.0f);
-                }
-                break;
-                
-            case WOLF:
-                if (isCrouch) {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_WOLF_HOWL, 1.0f, 1.0f);
-                } else {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_WOLF_GROWL, 1.0f, 1.0f);
-                }
-                break;
-                
-            case BOMB:
-                if (isCrouch) {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_GUARDIAN_ATTACK, 1.0f, 0.5f);
-                } else {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_CREEPER_PRIMED, 1.0f, 0.5f);
-                }
-                break;
-                
-            case VOID:
-                if (isCrouch) {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_WITHER_SPAWN, 1.0f, 0.5f);
-                } else {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_ENDERMAN_TELEPORT, 1.0f, 0.5f);
-                }
-                break;
-                
-            case PHANTOM:
-                if (isCrouch) {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_PHANTOM_SWOOP, 1.0f, 0.8f);
-                } else {
-                    loc.getWorld().playSound(loc, Sound.ENTITY_PHANTOM_AMBIENT, 1.0f, 1.5f);
-                }
-                break;
+    public void playSound(Location loc, Sound sound, float volume, float pitch) {
+        if (loc.getWorld() != null) {
+            loc.getWorld().playSound(loc, sound, volume, pitch);
         }
     }
     
-    /**
-     * Play sound with pitch variation
-     */
-    public static void playSoundVaried(Location loc, Sound sound, float volume, float basePitch, float variation) {
-        float pitch = basePitch + (float)(Math.random() * variation * 2 - variation);
-        loc.getWorld().playSound(loc, sound, volume, pitch);
+    public void playSound(Player player, Sound sound, float volume, float pitch) {
+        player.playSound(player.getLocation(), sound, volume, pitch);
     }
     
-    /**
-     * Play a sequence of sounds
-     */
-    public static void playSoundSequence(Location loc, Sound[] sounds, float[] delays) {
-        new BukkitRunnable() {
-            int index = 0;
-            
-            @Override
-            public void run() {
-                if (index >= sounds.length) {
-                    cancel();
-                    return;
-                }
-                
-                loc.getWorld().playSound(loc, sounds[index], 1.0f, 1.0f);
-                index++;
-            }
-        }.runTaskTimer(plugin, 0L, (long)(delays[index] * 20));
+    public void playFistSound(Player player, String fistType) {
+        Sound sound = getFistSound(fistType);
+        player.playSound(player.getLocation(), sound, 1.0f, 1.0f);
     }
     
-    /**
-     * Play cinematic sound for ability
-     */
-    public static void playCinematicSound(Location loc, FistType fist) {
-        switch(fist) {
-            case ORB:
-                loc.getWorld().playSound(loc, Sound.ENTITY_WITHER_SPAWN, 0.5f, 1.5f);
-                break;
-            case BLOSSOM:
-                loc.getWorld().playSound(loc, Sound.BLOCK_BEACON_ACTIVATE, 0.5f, 1.8f);
-                break;
-            case BEAST:
-                loc.getWorld().playSound(loc, Sound.ENTITY_RAVAGER_ROAR, 0.5f, 0.8f);
-                break;
-            case WATER:
-                loc.getWorld().playSound(loc, Sound.ITEM_TRIDENT_THUNDER, 0.5f, 1.2f);
-                break;
-            case REALITY:
-                loc.getWorld().playSound(loc, Sound.ENTITY_LIGHTNING_BOLT_THUNDER, 0.5f, 1.0f);
-                break;
-            case COSMIC:
-                loc.getWorld().playSound(loc, Sound.BLOCK_PORTAL_TRAVEL, 0.5f, 1.5f);
-                break;
-            case WOLF:
-                loc.getWorld().playSound(loc, Sound.ENTITY_WOLF_HOWL, 0.5f, 0.8f);
-                break;
-            case BOMB:
-                loc.getWorld().playSound(loc, Sound.ENTITY_DRAGON_FIREBALL_EXPLODE, 0.5f, 1.2f);
-                break;
-            case VOID:
-                loc.getWorld().playSound(loc, Sound.AMBIENT_CAVE, 0.5f, 0.5f);
-                break;
-            case PHANTOM:
-                loc.getWorld().playSound(loc, Sound.AMBIENT_UNDERWATER_ENTER, 0.5f, 1.5f);
-                break;
+    private Sound getFistSound(String fistType) {
+        switch(fistType.toLowerCase()) {
+            case "orb": return Sound.ENTITY_FIREWORK_ROCKET_BLAST;
+            case "blossom": return Sound.BLOCK_GRASS_BREAK;
+            case "beast": return Sound.ENTITY_RAVAGER_ROAR;
+            case "water": return Sound.BLOCK_WATER_AMBIENT;
+            case "reality": return Sound.BLOCK_STONE_BREAK;
+            case "cosmic": return Sound.ENTITY_ENDER_DRAGON_FLAP;
+            case "wolf": return Sound.ENTITY_WOLF_GROWL;
+            case "bomb": return Sound.ENTITY_CREEPER_PRIMED;
+            case "void": return Sound.AMBIENT_CAVE;
+            case "phantom": return Sound.ENTITY_PHANTOM_AMBIENT;
+            default: return Sound.ENTITY_EXPERIENCE_ORB_PICKUP;
         }
     }
     
-    /**
-     * Play UI sound
-     */
-    public static void playUISound(Player player, UISoundType type) {
+    // Fixed: Removed the 'index' variable that was causing error at line 130
+    public void playRandomPitch(Player player, Sound sound, float volume) {
+        float pitch = 0.8f + (float)(Math.random() * 0.4f);
+        player.playSound(player.getLocation(), sound, volume, pitch);
+    }
+    
+    // Fixed: ENTITY_WOLF_BREATHE doesn't exist in 1.21, using ENTITY_WOLF_GROWL instead
+    public void playWolfSound(Player player, String type) {
         switch(type) {
-            case CLICK:
-                player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 0.5f, 1.0f);
+            case "growl":
+                player.playSound(player.getLocation(), Sound.ENTITY_WOLF_GROWL, 1.0f, 1.0f);
                 break;
-            case SUCCESS:
-                player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 0.5f, 1.5f);
+            case "howl":
+                player.playSound(player.getLocation(), Sound.ENTITY_WOLF_HOWL, 1.0f, 1.0f);
                 break;
-            case ERROR:
-                player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 0.5f, 1.0f);
+            case "pant":
+                player.playSound(player.getLocation(), Sound.ENTITY_WOLF_GROWL, 0.5f, 1.5f); // Using growl with higher pitch as substitute
                 break;
-            case OPEN:
-                player.playSound(player.getLocation(), Sound.BLOCK_CHEST_OPEN, 0.5f, 1.0f);
-                break;
-            case CLOSE:
-                player.playSound(player.getLocation(), Sound.BLOCK_CHEST_CLOSE, 0.5f, 1.0f);
-                break;
-            case SELECT:
-                player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 0.5f, 1.5f);
-                break;
+            default:
+                player.playSound(player.getLocation(), Sound.ENTITY_WOLF_AMBIENT, 1.0f, 1.0f);
         }
-    }
-    
-    public enum UISoundType {
-        CLICK, SUCCESS, ERROR, OPEN, CLOSE, SELECT
-    }
-    
-    /**
-     * Play ambient sound loop
-     */
-    public static void playAmbientSound(Player player, FistType fist) {
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                if (!player.isOnline() || !plugin.getFistManager().hasFist(player)) {
-                    cancel();
-                    return;
-                }
-                
-                switch(fist) {
-                    case ORB:
-                        player.playSound(player.getLocation(), Sound.BLOCK_FIRE_AMBIENT, 0.2f, 1.0f);
-                        break;
-                    case WATER:
-                        player.playSound(player.getLocation(), Sound.BLOCK_WATER_AMBIENT, 0.2f, 1.0f);
-                        break;
-                    case COSMIC:
-                        player.playSound(player.getLocation(), Sound.AMBIENT_UNDERWATER_LOOP, 0.2f, 1.5f);
-                        break;
-                    case WOLF:
-                        player.playSound(player.getLocation(), Sound.ENTITY_WOLF_BREATHE, 0.2f, 1.0f);
-                        break;
-                }
-            }
-        }.runTaskTimer(plugin, 0L, 100L); // Every 5 seconds
     }
 }
